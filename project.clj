7217389a7f8963ac2c01e4fbd4470a14b7af9b7c
npatten <nikkopatten@gmail.com>
2014-02-01 01:07:43 -0500

    Back to working state
    Hoop generator rides one more.
    Still only responds to provided first name
    hitting ‘/‘ without params gets you a random hooper name.
    
    - Also had to fix project.clj : tools.reader had the wrong path.

--- project.clj
+++ project.clj
@@ -8,6 +8,6 @@
                  [ring/ring-jetty-adapter "1.1.6"]
                  [compojure "1.0.2"]
                  [ring/ring-devel "1.1.6"]
-                 [clojure.tools.reader "0.8.3"]]
+                 [org.clojure/tools.reader "0.8.3"]]
   :plugins [[lein-ring "0.7.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[com.cemerick/pomegranate "0.2.0"]]}})
--- src/hoops/core.clj
+++ src/hoops/core.clj
@@ -6,28 +6,42 @@
             [clojure.tools.reader :as rdr]
             [compojure.handler :refer (api)]))
 
+;;; Hooper Name Generator
+;;; 
+;;; 
+
+
+
 (def hooper-map
+  ;; Map of hooper first and last names
   {:fname ["Hooper" "Luna" "Dizzy" "Hooped" "Tinker"]
    :lname ["Star" "Hoops" "Galloopsie"]})
+
 
 (defn full-hoop-name [fname lname]
   (str fname " " lname))
 
+;; rand-name: fname | lname -> String
 (defn rand-name [t] (rand-nth (t hooper-map)))
 
-(defn matching-name? [name first-or-last]
-  (seq (filter #(= (first %) (first fname))
-)))
+ 
+(defn matching-name? [name]
+  ;; Randomly pick a name from the subset of hooper first names that begin with the same-
+  ;;  letter as [name]
+  (seq (filter #(= (first %) (first name)) (hooper-map :fname))))
+
 
 (defn build-hooper-name [{fname :fname, lname :lname
-                          :or {fname (rand-fname), lname (rand-lname)}}]
+                          :or {fname (rand-name :fname), lname (rand-name :lname)}}]
+;; Some fancyness to default to a random first or last hoop name if one isn't provided.
   (full-hoop-name fname lname))
 
-(defn determine-hoop-name [fname]
-  (let [matching-hoop-names (filter #(= (first %) (first fname)) hooper-first-names)]
-    (if (matching-name?)
-      (full-hoop-name (-> matching-hoop-names rand-nth) (rand-lname))
-      (build-hooper-name fname))))
+
+(defn determine-hoop-name [names]
+  (let [matching-hoop-names (matching-name? (:fname names))]
+       (if matching-hoop-names
+         (full-hoop-name (-> matching-hoop-names rand-nth) (rand-name :lname))
+         (build-hooper-name names))))
 
 (defn query-params [ctx]
   (get-in ctx [:request :query-params]))
@@ -38,14 +52,14 @@
                (let [params (query-params ctx)
                      {:strs [firstname]} params]
                  (if firstname
-                   (determine-hoop-name firstname)
-                   (generate-hooper-name {:edn true})))))
+                   (determine-hoop-name  {:fname firstname})
+                   (build-hooper-name {})))))
+
 
 (defn all-routes []
   (routes (ANY "/" {} hoop-name)))
 
 (def handler
-
   (-> (all-routes)
       api
       wrap-multipart-params))
@@ -56,10 +70,12 @@
 (defn -main []
   (start {:port 3000}))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (comment
   (generate-hooper-name)
   (require '[cemerick.pomegranate :as pom])
   (require '[cemerick.pomegranate.aether :as aether])
   (pom/add-dependencies :coordinates '[[clojure.tools.reader "0.8.3"]]
                         :repositories (merge aether/maven-central {"clojars" "http://clojars.org/repo"}))
-)
+  )
